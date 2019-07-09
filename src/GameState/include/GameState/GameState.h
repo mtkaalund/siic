4fa0a9c41b04506a9c299828394be2392a4d5d39
Michael Torp Kaalund <kaalund@gmail.com>
2019-07-10 00:31:16 +0200

    put gameengine in namespace

--- src/GameState/include/GameState/GameState.h
+++ src/GameState/include/GameState/GameState.h
@@ -9,14 +9,14 @@ class GameEngine;
 
 class GameState {
 public:
-    virtual void init( GameEngine * game )              = 0;
-    virtual void cleanup()                              = 0;
-    virtual void pause()                                = 0;
-    virtual void unpause()                              = 0;
+    virtual void init( siic::GameEngine * game )              = 0;
+    virtual void cleanup()                                    = 0;
+    virtual void pause()                                      = 0;
+    virtual void unpause()                                    = 0;
 
-    virtual void handle_events( GameEngine * game )     = 0;
-    virtual void update( GameEngine * game )            = 0;
-    virtual void render( GameEngine * game )            = 0;
+    virtual void handle_events( siic::GameEngine * game )     = 0;
+    virtual void update( siic::GameEngine * game )            = 0;
+    virtual void render( siic::GameEngine * game )            = 0;
 
     void change_state( GameEngine * game, GameState * state );
 protected:
